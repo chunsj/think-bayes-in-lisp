9ab9c9f618f0dde2ec6fbfd7ef1d0fe269d8480e
Sungjin Chun <chunsj@gmail.com>
2018-02-27 20:33:44 +0900

    dice example

---
+++ scratch/dice.lisp
@@ -0,0 +1,17 @@
+(in-package :think-bayes)
+
+(defclass dice (suite) ())
+
+(defmethod likelihood ((self dice) data hypothesis)
+  (if (< hypothesis data)
+      0.0
+      (/ 1.0 hypothesis)))
+
+(let ((suite (create-suite 'dice '(4 6 8 12 20))))
+  (update suite 6)
+  suite)
+
+(let ((suite (create-suite 'dice '(4 6 8 12 20))))
+  (update suite 6)
+  (loop :for roll :in '(6 8 7 7 5 4) :do (update suite roll))
+  suite)
