1284b79a0ab4467d77dc5462f1d91dd1abe1e1b6
Sungjin Chun <chunsj@gmail.com>
2018-03-03 23:11:25 +0900

    added more generic to-pdf and working on price

--- scratch/price-is-right.lisp
+++ scratch/price-is-right.lisp
@@ -30,3 +30,62 @@
 (-> (to-pmf (empirical ($ *showcase2012* :showcase1))
             :xs (linspace 0 75000 101))
     (plot :xtics 10))
+
+(-> (to-pmf (empirical ($ *showcase2011* :difference1))
+            :xs (linspace -50000 50000 101))
+    (to-cdf)
+    (plot :xtics 10))
+
+(-> (to-pmf (empirical ($ *showcase2011* :difference2))
+            :xs (linspace -50000 50000 101))
+    (to-cdf)
+    (plot :xtics 10))
+
+(defclass player ()
+  ((pdf-price :initform nil :accessor pdf-price)
+   (cdf-diff :initform nil :accessor cdf-diff)
+   (pdf-error :initform nil :accessor pdf-error)
+   (n :initform 101 :accessor player-steps)
+   (price-xs :initform (linspace 0 75000 101) :accessor price-xs)
+   (diff-xs :initform (linspace -50000 50000 101) :accessor diff-xs)
+   (prior :initform nil :accessor player-prior)
+   (posterior :initform nil :accessor player-posterior)))
+
+(defun player (prices diffs)
+  (let ((instance (make-instance 'player))
+        (price-emp (empirical prices))
+        (diff-emp (empirical diffs)))
+    (setf (pdf-price instance) price-emp)
+    (setf (cdf-diff instance) (-> diff-emp
+                                  (to-pmf :xs (diff-xs instance))
+                                  (to-cdf)))
+    (setf (pdf-error instance) (gaussian :sigma (sqrt (xvariance diff-emp))))
+    instance))
+
+(defgeneric error-density (player error))
+(defmethod error-density ((player player) error) (p (pdf-error player) error))
+
+(defclass price (pmf)
+  ((player :initform nil :accessor price-player)))
+
+(defun price (pmf player)
+  (let ((price (pmf :class 'price)))
+    (setf (xpmap price) (xpmap pmf))
+    (setf (price-player price) player)
+    price))
+
+(defmethod likelihood ((price price) evidence hypo)
+  (let ((prc hypo)
+        (guess evidence))
+    (error-density (price-player price) (- prc guess))))
+
+(defgeneric make-beliefs (player guess))
+(defmethod make-beliefs ((player player) guess)
+  (let ((pmf (to-pmf (pdf-price player) :xs (price-xs player))))
+    (setf (player-prior player) (price pmf player))
+    (setf (player-posterior player) (price pmf player))
+    (update (player-posterior player) guess)))
+
+(let ((player (player ($ *showcase2011* :showcase1) ($ *showcase2011* :difference1))))
+  (make-beliefs player 20000)
+  (plot (player-posterior player) :xtics 10))
