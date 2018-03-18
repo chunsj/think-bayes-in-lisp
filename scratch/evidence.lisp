44697b7ed649a59c014b8f50a002330d8e39a8a5
Sungjin Chun <chunsj@gmail.com>
2018-03-18 13:25:28 +0900

    some memo on church style work

--- scratch/evidence.lisp
+++ scratch/evidence.lisp
@@ -1,1 +1,11 @@
 (in-package :think-bayes)
+
+;; 1. need a histogram creator
+;; 2. flip
+;; 3. density (which is normalized (0 to 1) histogram (related to think bayes style)
+(defun flip (&optional (prob 0.5D0))
+  (if (< (random 1D0) prob)
+      1
+      0))
+
+(repeat 10 (lambda () (flip 0.1)))
